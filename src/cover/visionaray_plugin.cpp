303e938c21d6dcde2f653a7c36ca046bbce1410d
Stefan Zellmann <info@szellmann.de>
2015-02-25 16:24:01 +0100

    Experimental, very simplistic OpenCOVER plugin
    Requires: covise: https://github.com/hlrs-vis/covise
    
    For building (tested on Linux):
    source covise/.covise.sh (pulls $COVISEDIR into env)
    
    Adapt src/visionaray/CMakeLists.txt:154 so that a shared library is built
    Adapt src/cover/CMakeLists.txt:3 to point to the location where libvisionaray.so is built
    Then envoke cmake and build visionaray as usual

---
+++ src/cover/visionaray_plugin.cpp
@@ -0,0 +1,304 @@
+// This file is distributed under the MIT license.
+// See the LICENSE file for details.
+
+#include <cassert>
+
+#include <iostream>
+#include <limits>
+#include <ostream>
+
+#include <GL/glew.h>
+
+#include <osg/MatrixTransform>
+#include <osg/StateSet>
+#include <osg/TriangleFunctor>
+
+#include <cover/coVRPluginSupport.h>
+#include <cover/VRViewer.h>
+
+#include <visionaray/detail/aligned_vector.h>
+#include <visionaray/math/math.h>
+#include <visionaray/bvh.h>
+#include <visionaray/kernels.h>
+#include <visionaray/point_light.h>
+#include <visionaray/render_target.h>
+#include <visionaray/scheduler.h>
+
+#include "visionaray_plugin.h"
+
+namespace visionaray { namespace cover {
+
+
+//-------------------------------------------------------------------------------------------------
+// Type definitions
+//
+
+using triangle_type     = basic_triangle<3, float>;
+using triangle_list     = aligned_vector<triangle_type>;
+using normal_list       = aligned_vector<vec3>;
+
+using host_ray_type     = basic_ray<simd::float4>;
+using host_bvh_type     = bvh<triangle_type>;
+using host_sched_type   = tiled_sched<host_ray_type>;
+
+
+//-------------------------------------------------------------------------------------------------
+// Functor that stores triangles from osg::Drawable
+//
+
+class store_triangle
+{
+public:
+
+    store_triangle() : triangles_(nullptr) {}
+
+    void set_lists(triangle_list& tris, normal_list& norms)
+    {
+        triangles_  = &tris;
+        normals_    = &norms;
+    }
+
+    void operator()(osg::Vec3 const& v1, osg::Vec3 const& v2, osg::Vec3 const& v3, bool) const
+    {
+        assert( triangles_ && normals_ );
+
+        triangle_type tri;
+        tri.prim_id = static_cast<unsigned>(triangles_->size());
+        tri.geom_id = 0;
+        tri.v1 = vec3(v1.x(), v1.y(), v1.z());
+        tri.e1 = vec3(v2.x(), v2.y(), v2.z()) - tri.v1;
+        tri.e2 = vec3(v3.x(), v3.y(), v3.z()) - tri.v1;
+        triangles_->push_back(tri);
+
+        normals_->push_back( normalize(cross(tri.e1, tri.e2)) );
+
+        assert( triangles_->size() == normals_->size() );
+    }
+
+private:
+
+    // Store pointers because osg::TriangleFunctor is shitty..
+    triangle_list*  triangles_;
+    normal_list*    normals_;
+
+};
+
+
+//-------------------------------------------------------------------------------------------------
+// Visitor to acquire scene data
+//
+
+class get_scene_visitor : public osg::NodeVisitor
+{
+public:
+
+    using base_type = osg::NodeVisitor;
+    using base_type::apply;
+
+    get_scene_visitor(triangle_list& tris, normal_list& norms, TraversalMode tm)
+        : base_type(tm)
+        , triangles_(tris)
+        , normals_(norms)
+    {
+    }
+
+    void apply(osg::Geode& geode)
+    {
+        for (size_t i = 0; i < geode.getNumDrawables(); ++i)
+        {
+            auto drawable = geode.getDrawable(i);
+            if (drawable)
+            {
+                osg::TriangleFunctor<store_triangle> tf;
+                tf.set_lists(triangles_, normals_);
+                drawable->accept(tf);
+            }
+        }
+
+        base_type::traverse(geode);
+    }
+
+private:
+
+    triangle_list&  triangles_;
+    normal_list&    normals_;
+
+};
+
+
+//-------------------------------------------------------------------------------------------------
+// Private implementation
+//
+
+struct Visionaray::impl
+{
+    impl()
+        : visitor(triangles, normals, osg::NodeVisitor::TRAVERSE_ALL_CHILDREN)
+    {
+    }
+
+    triangle_list                       triangles;
+    normal_list                         normals;
+    host_bvh_type                       host_bvh;
+    host_sched_type                     host_sched;
+    cpu_buffer_rt                       host_rt;
+
+    recti                               viewport;
+
+    get_scene_visitor                   visitor;
+    osg::ref_ptr<osg::MatrixTransform>  transform;
+    osg::ref_ptr<osg::Geode>            geode;
+};
+
+
+Visionaray::Visionaray()
+    : impl_(new impl)
+{
+    setSupportsDisplayList(false);
+}
+
+Visionaray::Visionaray(Visionaray const& rhs, osg::CopyOp const& op)
+    : Drawable(rhs, op)
+    , impl_(new impl)
+{
+    setSupportsDisplayList(false);
+}
+
+Visionaray::~Visionaray()
+{
+    impl_->transform->removeChild(impl_->geode);
+    opencover::cover->getObjectsRoot()->removeChild(impl_->transform);
+}
+
+bool Visionaray::init()
+{
+    using namespace osg;
+
+    opencover::VRViewer::instance()->culling(false);
+
+    std::cout << "Init Visionaray Plugin!!" << std::endl;
+
+    ref_ptr<osg::StateSet> state = new osg::StateSet();
+    state->setGlobalDefaults();
+
+    impl_->geode = new osg::Geode;
+    impl_->geode->setStateSet(state);
+    impl_->geode->addDrawable(this);
+
+    impl_->transform = new osg::MatrixTransform();
+    impl_->transform->addChild(impl_->geode);
+
+    opencover::cover->getObjectsRoot()->addChild(impl_->transform);
+
+    return true;
+}
+
+void Visionaray::preFrame()
+{
+}
+
+void Visionaray::drawImplementation(osg::RenderInfo&) const
+{
+    // TODO?
+
+    static bool glewed = false;
+
+    if (!glewed)
+    {
+        glewed = glewInit() == GLEW_OK;
+    }
+
+
+    // Scene data
+
+    if (impl_->triangles.size() == 0)
+    {
+        // TODO: no dynamic scenes for now :(
+        impl_->visitor.apply(*opencover::cover->getObjectsRoot());
+
+        if (impl_->triangles.size() == 0)
+        {
+            return;
+        }
+        impl_->host_bvh = build<host_bvh_type>(impl_->triangles.data(), impl_->triangles.size());
+    }
+
+    aabb bounds( vec3(std::numeric_limits<float>::max()), -vec3(std::numeric_limits<float>::max()) );
+    for (auto const& tri : impl_->triangles)
+    {
+        auto v1 = tri.v1;
+        auto v2 = tri.v1 + tri.e1;
+        auto v3 = tri.v1 + tri.e2;
+
+        bounds = combine(bounds, aabb(v1, v1));
+        bounds = combine(bounds, aabb(v2, v2));
+        bounds = combine(bounds, aabb(v3, v3));
+    }
+
+
+    // Sched params
+
+    GLfloat view[16];
+    glGetFloatv(GL_MODELVIEW_MATRIX, view);
+
+    GLfloat proj[16];
+    glGetFloatv(GL_PROJECTION_MATRIX, proj);
+
+    GLint vp[4] = { 0, 0, 0, 0 };
+    glGetIntegerv(GL_VIEWPORT, vp);
+
+    mat4 view_matrix(view);
+    mat4 proj_matrix(proj);
+    recti viewport(vp[0], vp[1], vp[2], vp[3]);
+
+    auto sparams = make_sched_params<pixel_sampler::uniform_type>( view_matrix, proj_matrix, viewport, impl_->host_rt );
+
+    if (impl_->viewport != viewport)
+    {
+        impl_->host_rt.resize(vp[2], vp[3]);
+        impl_->viewport = viewport;
+    }
+
+    // Kernel params
+
+    aligned_vector<host_bvh_type::bvh_ref> host_primitives;
+    host_primitives.push_back(impl_->host_bvh.ref());
+
+    aligned_vector<phong<float>> materials;
+    phong<float> m;
+    m.set_cd( vec3(0.8f, 0.8f, 0.8f) );
+    m.set_kd( 1.0f );
+    m.set_ks( 1.0f );
+    m.set_specular_exp( 32.0f );
+    materials.push_back(m);
+
+    vec4 lpos;
+    glGetLightfv(GL_LIGHT0, GL_POSITION, lpos.data());
+
+    aligned_vector<point_light<float>> lights;
+    vec3 light0_pos = (inverse(view_matrix) * lpos).xyz();
+    lights.push_back({ light0_pos });
+
+
+    auto kparams = make_params
+    (
+        host_primitives.data(),
+        host_primitives.data() + host_primitives.size(),
+        impl_->normals.data(),
+        materials.data(),
+        lights.data(),
+        lights.data() + lights.size()
+    );
+
+    // Render
+
+    auto kern =  simple::kernel<vector<4, simd::float4>, decltype(kparams)>();
+    kern.params = kparams;
+    impl_->host_sched.frame(kern, sparams);
+
+    impl_->host_rt.display_color_buffer();
+}
+
+}} // namespace visionaray::cover
+
+COVERPLUGIN(visionaray::cover::Visionaray)
---
+++ src/cover/visionaray_plugin.h
@@ -0,0 +1,49 @@
+// This file is distributed under the MIT license.
+// See the LICENSE file for details.
+
+#ifndef VSNRAY_COVER_PLUGIN_H
+#define VSNRAY_COVER_PLUGIN_H
+
+#include <memory>
+
+#include <cover/coVRPlugin.h>
+
+namespace visionaray { namespace cover
+{
+
+class Visionaray : public opencover::coVRPlugin, public osg::Drawable
+{
+public:
+
+    Visionaray();
+   ~Visionaray();
+
+    bool init();
+    void preFrame();
+
+private:
+
+    struct impl;
+    std::unique_ptr<impl> impl_;
+
+private:
+
+    Visionaray* cloneType() const
+    {
+        return new Visionaray;
+    }
+
+    osg::Object* clone(const osg::CopyOp& op) const
+    {
+        return new Visionaray(*this, op);
+    }
+
+    Visionaray(Visionaray const& rhs, osg::CopyOp const& op = osg::CopyOp::SHALLOW_COPY);
+
+    void drawImplementation(osg::RenderInfo& info) const;
+
+};
+
+}} // namespace visionaray::cover
+
+#endif // VSNRAY_COVER_PLUGIN_H
