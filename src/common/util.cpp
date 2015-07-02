964416071cb1670fdb2bde0ffa71dfb75e0db6e8
abolz <lt.morris.schaffer@googlemail.com>
2015-07-02 13:09:02 +0200

    Implement get_num_processors in cpp file

---
+++ src/common/util.cpp
@@ -0,0 +1,27 @@
+// This file is distributed under the MIT license.
+// See the LICENSE file for details.
+
+#include <visionaray/detail/macros.h>
+
+#if defined(VSNRAY_OS_WIN32)
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#include "util.h"
+
+unsigned visionaray::get_num_processors()
+{
+#if defined(VSNRAY_OS_WIN32)
+
+    SYSTEM_INFO sysinfo;
+    GetNativeSystemInfo(&sysinfo);
+    return static_cast<unsigned>(sysinfo.dwNumberOfProcessors);
+
+#else
+
+    return static_cast<unsigned>(sysconf(_SC_NPROCESSORS_ONLN));
+
+#endif
+}
