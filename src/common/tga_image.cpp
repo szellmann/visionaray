2f5448a93ea5ccd29e18f67b0aff253144b6c946
Stefan Zellmann <info@szellmann.de>
2015-08-23 01:19:19 +0200

    Support for run-length encoded TGA images

--- src/common/tga_image.cpp
+++ src/common/tga_image.cpp
@@ -59,32 +59,91 @@ pixel_format map_pixel_depth(uint8_t bits_per_pixel)
 }
 
 
-void load_true_color(
-        uint8_t* dst,
-        std::ifstream& file,
-        int stride,
-        int height,
-        int y_origin
+void load_true_color_uncompressed(
+        uint8_t*        dst,
+        std::ifstream&  file,
+        int             width,
+        int             height,
+        int             bytes_per_pixel,
+        int             y_origin
         )
 {
+    int pitch = width * bytes_per_pixel;
+
     if (y_origin == 0)
     {
         // Origin is bottom/left corner - same as visionaray image format
-        file.read(reinterpret_cast<char*>(dst), stride * height * sizeof(uint8_t));
+        file.read(reinterpret_cast<char*>(dst), pitch * height * sizeof(uint8_t));
     }
     else if (y_origin == height)
     {
         // Origin is top/left corner - convert to bottom/left
         for (int y = 0; y < height; ++y)
         {
-            auto ptr = dst + (height - 1) * stride - y * stride;
-            file.read(reinterpret_cast<char*>(ptr), stride * sizeof(uint8_t));
+            auto ptr = dst + (height - 1) * pitch - y * pitch;
+            file.read(reinterpret_cast<char*>(ptr), pitch * sizeof(uint8_t));
         }
     }
     else
     {
         std::cerr << "Unsupported TGA image, y-origin ("
                   << y_origin << ") is neither bottom nor top\n";
+    }
+}
+
+
+void load_true_color_rle(
+        uint8_t*        dst,
+        std::ifstream&  file,
+        int             width,
+        int             height,
+        int             bytes_per_pixel,
+        int             y_origin
+        )
+{
+    if (y_origin != 0 && y_origin != height)
+    {
+        std::cerr << "Unsupported TGA image, y-origin ("
+                  << y_origin << ") is neither bottom nor top\n";
+        return;
+    }
+
+    int pixels = 0;
+
+    while (pixels < width * height)
+    {
+        uint8_t chunk_header = 0;
+        file.read((char*)&chunk_header, sizeof(chunk_header));
+
+        int current_pixel = pixels; // true if y-origin = 0
+        if (y_origin == height)
+        {
+            int x = pixels % width;
+            int y = pixels / width;
+            current_pixel = (height - 1) * width - y * width + x;
+        }
+        auto ptr = dst + current_pixel * bytes_per_pixel;
+
+        if (chunk_header < 128)
+        {
+            ++chunk_header;
+            file.read(reinterpret_cast<char*>(ptr), chunk_header * bytes_per_pixel);
+        }
+        else
+        {
+            chunk_header -= 127;
+            char buffer[4];
+            file.read(buffer, bytes_per_pixel);
+            for (uint8_t i = 0; i < chunk_header; ++i)
+            {
+                for (int b = 0; b < bytes_per_pixel; ++b)
+                {
+                    *(ptr + i * bytes_per_pixel + b) = buffer[b];
+                }
+            }
+        }
+
+        pixels += chunk_header;
     }
 }
 
@@ -108,8 +167,8 @@ tga_image::tga_image(std::string const& filename)
     width_  = static_cast<size_t>(header.width);
     height_ = static_cast<size_t>(header.height);
     format_ = map_pixel_depth(header.bits_per_pixel);
-    auto stride = header.width * (header.bits_per_pixel / 8);
-    data_.resize(stride * header.height);
+    auto pitch = header.width * (header.bits_per_pixel / 8);
+    data_.resize(pitch * header.height);
 
 
     // Read image data
@@ -129,7 +188,25 @@ tga_image::tga_image(std::string const& filename)
         break;
 
     case 2:
-        load_true_color(data_.data(), file, stride, header.height, header.y_origin);
+        load_true_color_uncompressed(
+                data_.data(),
+                file,
+                header.width,
+                header.height,
+                header.bits_per_pixel / 8,
+                header.y_origin
+                );
+        break;
+
+    case 10:
+        load_true_color_rle(
+                data_.data(),
+                file,
+                header.width,
+                header.height,
+                header.bits_per_pixel / 8,
+                header.y_origin
+                );
         break;
 
     }
