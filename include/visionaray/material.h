c76e6ce23eac86266faf7bb2b9e426851162bc20
Stefan Zellmann <info@szellmann.de>
2016-01-19 13:16:51 +0100

    Documentation for materials

--- include/visionaray/material.h
+++ include/visionaray/material.h
@@ -18,7 +18,68 @@ namespace visionaray
 {
 
 //-------------------------------------------------------------------------------------------------
+// Material classes
+//
+//
+// Built-in and user-defined materials (must) support the following interface:
+//
+//  - shade():
+//      const parameter shade_record:   shading info (normal, texture color, ...)
+//      return type:                    spectrum
+//
+//  - sample():
+//      const parameter shade_record:   shading info (normal, texture color, ...)
+//      modifiable parameter refl_dir:  outgoing light direction (computed by function,
+//                                      must be normalized)
+//      modifiable parameter pdf:       probability density function of the the sampled BRDF
+//      modifiable parameter sampler:   implements sampler interface to get pseudo random
+//                                      numbers or quasi random numbers
+//
+//
+// Built-in materials
+//
+//  - emissive:
+//      shade() returns radiance emitted by the surface
+//      mind that radiance for emitters beyond 1.0 cd/m^2 is physically plausible
+//      no reflective properties, sample() will call no valid reflection
+//
+//  - matte:
+//      material with only diffuse reflection properties
+//
+//  - mirror:
+//      material with only perfectly specular reflection properties
+//
+//  - plastic:
+//      OpenGL-like material with a diffuse BRDF (Lambertian reflection) and a specular BRDF
+//      (Blinn reflection model). When being sampled, the diffuse and specular terms
+//      are used to determine whether the diffuse or specular BRDF will contribute:
+//
+//          Pd = mean_value(cd) * kd        /* probability of diff. reflection */
+//          Ps = mean_value(cs) * ks        /* probability of spec. reflection */
+//
+//          Pd = Pd / (Pd + Ps)             /* normalization */
+//          Ps = Ps / (Pd + Ps)
+//
+//
+// Compatibility with generic_material<Ts...>
+//
+//  User-defined materials are compatible with the generic_material mechanism if
+// they implement the interface described above [...]
+//
+//  TODO: make variant-type more versatile:
+//
+//  [...] and if they contain no constructors other than the compiler-generated
+//  constructors. The same applies do destructors and assignment operators.
+//
+//-------------------------------------------------------------------------------------------------
+
+
+//-------------------------------------------------------------------------------------------------
 // Emissive material, no BRDFs
+//
+// Parameters:
+//  - ce: emissive spectrum
+//  - ls: light intensity
 //
 
 template <typename T>
@@ -64,6 +125,10 @@ private:
 
 //-------------------------------------------------------------------------------------------------
 // Matte material
+//
+// Parameters:
+//  - cd: diffuse spectrum
+//  - kd: diffuse reflection coefficient (scales cd)
 //
 
 template <typename T>
@@ -136,6 +201,12 @@ private:
 //-------------------------------------------------------------------------------------------------
 // Mirror material
 //
+// Parameters:
+//  - cr:           perfect specular spectrum
+//  - kr:           perfect specular reflection coefficient (scales cr)
+//  - ior:          index of refraction for fresnel reflection
+//  - absorption:   amount of radiance absorpt by the material
+//
 
 template <typename T>
 class mirror
@@ -184,6 +255,13 @@ private:
 
 //-------------------------------------------------------------------------------------------------
 // Plastic material
+//
+// Parameters:
+//  - cd:           diffuse spectrum
+//  - kd:           diffuse reflection coefficient (scales cd)
+//  - cs:           specular spectrum
+//  - ks:           specular reflection coefficient (scales ks)
+//  - specular_exp: controls the cone in which light may be reflected, range: [0..inf]
 //
 
 template <typename T>
