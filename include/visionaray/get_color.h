56d00f74f2a5fae1b453bc9e4192070e7084791b
Stefan Zellmann <info@szellmann.de>
2016-02-16 10:45:50 +0100

    Implement get_color() for per-face colors

--- include/visionaray/get_color.h
+++ include/visionaray/get_color.h
@@ -15,6 +15,23 @@ namespace visionaray
 {
 
 //-------------------------------------------------------------------------------------------------
+// Get face color from array
+//
+
+template <typename Colors, typename HR, typename Primitive>
+VSNRAY_FUNC
+inline auto get_color(
+        Colors                  colors,
+        HR const&               hr,
+        Primitive               /* */,
+        colors_per_face_binding /* */
+        )
+{
+    return colors[hr.prim_id];
+}
+
+
+//-------------------------------------------------------------------------------------------------
 // Get triangle vertex color from array
 //
 
@@ -36,6 +53,93 @@ inline auto get_color(
             hr.v
             );
 }
+
+
+//-------------------------------------------------------------------------------------------------
+// Gather four face colors with SSE
+//
+
+template <
+    typename Colors,
+    template <typename, typename> class HR,
+    typename HRP,
+    typename Primitive
+    >
+inline vector<3, simd::float4> get_color(
+        Colors                      colors,
+        HR<simd::ray4, HRP> const&  hr,
+        Primitive                   /* */,
+        normals_per_face_binding    /* */
+        )
+{
+    using C = typename std::iterator_traits<Colors>::value_type;
+
+    auto hr4 = unpack(hr);
+
+    auto get_clr = [&](int x)
+    {
+        return hr4[x].hit ? colors[hr4[x].prim_id] : C();
+    };
+
+    auto c1 = get_clr(0);
+    auto c2 = get_clr(1);
+    auto c3 = get_clr(2);
+    auto c4 = get_clr(3);
+
+    return vector<3, simd::float4>(
+            simd::float4( c1.x, c2.x, c3.x, c4.x ),
+            simd::float4( c1.y, c2.y, c3.y, c4.y ),
+            simd::float4( c1.z, c2.z, c3.z. c4.z )
+            );
+}
+
+
+#if VSNRAY_SIMD_ISA >= VSNRAY_SIMD_ISA_AVX
+
+//-------------------------------------------------------------------------------------------------
+// Gather eight face colors with AVX
+//
+
+template <
+    typename Colors,
+    template <typename, typename> class HR,
+    typename HRP,
+    typename Primitive
+    >
+inline vector<3, simd::float8> get_color(
+        Colors                      colors,
+        HR<simd::ray8, HRP> const&  hr,
+        Primitive                   /* */,
+        normals_per_face_binding    /* */
+        )
+{
+    using C = typename std::iterator_traits<Colors>::value_type;
+
+    auto hr8 = unpack(hr);
+
+    auto get_clr = [&](int x)
+    {
+        return hr8[x].hit ? colors[hr8[x].prim_id] : C();
+    };
+
+    auto c1 = get_clr(0);
+    auto c2 = get_clr(1);
+    auto c3 = get_clr(2);
+    auto c4 = get_clr(3);
+    auto c5 = get_clr(4);
+    auto c6 = get_clr(5);
+    auto c7 = get_clr(6);
+    auto c8 = get_clr(7);
+
+    return vector<3, simd::float8>(
+            simd::float8( c1.x, c2.x, c3.x, c4.x, c5.x, c6.x, c7.x, c8.x ),
+            simd::float8( c1.y, c2.y, c3.y, c4.y, c5.y, c6.y, c7.y, c8.y ),
+            simd::float8( c1.z, c2.z, c3.z. c4.z, c5.z, c6.z, c7.z, c8.z )
+            );
+}
+
+#endif // VSNRAY_SIMD_ISA >= VSNRAY_SIMD_ISA_AVX
+
 
 //-------------------------------------------------------------------------------------------------
 // SSE triangle, colors per vertex
