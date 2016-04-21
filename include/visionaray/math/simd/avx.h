fdd8e5442b593930eb508609017702761a591b4a
Stefan Zellmann <info@szellmann.de>
2016-04-21 16:37:49 +0200

    No inline specifiers for declarations

--- include/visionaray/math/simd/avx.h
+++ include/visionaray/math/simd/avx.h
@@ -31,8 +31,8 @@ public:
     typedef __m256 value_type;
     __m256 value;
 
-    VSNRAY_FORCE_INLINE basic_float() = default;
-    VSNRAY_FORCE_INLINE basic_float(
+    basic_float() = default;
+    basic_float(
             float x1,
             float x2,
             float x3,
@@ -42,12 +42,12 @@ public:
             float x7,
             float x8
             );
-    VSNRAY_FORCE_INLINE basic_float(float const v[8]);
-    VSNRAY_FORCE_INLINE basic_float(float s);
-    VSNRAY_FORCE_INLINE basic_float(__m256i const& i);
-    VSNRAY_FORCE_INLINE basic_float(__m256 const& v);
+    basic_float(float const v[8]);
+    basic_float(float s);
+    basic_float(__m256i const& i);
+    basic_float(__m256 const& v);
 
-    VSNRAY_FORCE_INLINE operator __m256() const;
+    operator __m256() const;
 };
 
 
@@ -63,8 +63,8 @@ public:
     typedef __m256i value_type;
     __m256i value;
 
-    VSNRAY_FORCE_INLINE basic_int() = default;
-    VSNRAY_FORCE_INLINE basic_int(
+    basic_int() = default;
+    basic_int(
             int x1,
             int x2,
             int x3,
@@ -74,13 +74,13 @@ public:
             int x7,
             int x8
             );
-    VSNRAY_FORCE_INLINE basic_int(int const v[8]);
-    VSNRAY_FORCE_INLINE basic_int(int s);
-    VSNRAY_FORCE_INLINE basic_int(unsigned s);
-    VSNRAY_FORCE_INLINE basic_int(basic_float<__m256> const& f);
-    VSNRAY_FORCE_INLINE basic_int(__m256i const& v);
+    basic_int(int const v[8]);
+    basic_int(int s);
+    basic_int(unsigned s);
+    basic_int(basic_float<__m256> const& f);
+    basic_int(__m256i const& v);
 
-    VSNRAY_FORCE_INLINE operator __m256i() const;
+    operator __m256i() const;
 };
 
 
@@ -96,11 +96,11 @@ public:
     __m256  f;
     __m256i i;
 
-    VSNRAY_FORCE_INLINE basic_mask() = default;
-    VSNRAY_FORCE_INLINE basic_mask(__m256 const& m);
-    VSNRAY_FORCE_INLINE basic_mask(__m256i const& m);
-    VSNRAY_FORCE_INLINE basic_mask(bool b);
-    VSNRAY_FORCE_INLINE basic_mask(
+    basic_mask() = default;
+    basic_mask(__m256 const& m);
+    basic_mask(__m256i const& m);
+    basic_mask(bool b);
+    basic_mask(
             bool x1,
             bool x2,
             bool x3,
@@ -110,10 +110,10 @@ public:
             bool x7,
             bool x8
             );
-    VSNRAY_FORCE_INLINE basic_mask(bool const v[8]);
-    VSNRAY_FORCE_INLINE basic_mask(basic_float<__m256> const& m);
+    basic_mask(bool const v[8]);
+    basic_mask(basic_float<__m256> const& m);
 
-    VSNRAY_FORCE_INLINE operator basic_float<__m256>() const;
+    operator basic_float<__m256>() const;
 };
 
 } // simd
--- include/visionaray/math/simd/sse.h
+++ include/visionaray/math/simd/sse.h
@@ -30,14 +30,14 @@ public:
     typedef __m128 value_type;
     __m128 value;
 
-    VSNRAY_FORCE_INLINE basic_float() = default;
-    VSNRAY_FORCE_INLINE basic_float(float x, float y, float z, float w);
-    VSNRAY_FORCE_INLINE basic_float(float const v[4]);
-    VSNRAY_FORCE_INLINE basic_float(float s);
-    VSNRAY_FORCE_INLINE basic_float(__m128i const& i);
-    VSNRAY_FORCE_INLINE basic_float(__m128 const& v);
+    basic_float() = default;
+    basic_float(float x, float y, float z, float w);
+    basic_float(float const v[4]);
+    basic_float(float s);
+    basic_float(__m128i const& i);
+    basic_float(__m128 const& v);
 
-    VSNRAY_FORCE_INLINE operator __m128() const;
+    operator __m128() const;
 };
 
 
@@ -53,15 +53,15 @@ public:
     typedef __m128i value_type;
     __m128i value;
 
-    VSNRAY_FORCE_INLINE basic_int() = default;
-    VSNRAY_FORCE_INLINE basic_int(int x, int y, int z, int w);
-    VSNRAY_FORCE_INLINE basic_int(int const v[4]);
-    VSNRAY_FORCE_INLINE basic_int(int s);
-    VSNRAY_FORCE_INLINE basic_int(unsigned s);
-    VSNRAY_FORCE_INLINE basic_int(basic_float<__m128> const& f);
-    VSNRAY_FORCE_INLINE basic_int(__m128i const& v);
+    basic_int() = default;
+    basic_int(int x, int y, int z, int w);
+    basic_int(int const v[4]);
+    basic_int(int s);
+    basic_int(unsigned s);
+    basic_int(basic_float<__m128> const& f);
+    basic_int(__m128i const& v);
 
-    VSNRAY_FORCE_INLINE operator __m128i() const;
+    operator __m128i() const;
 };
 
 
@@ -77,15 +77,15 @@ public:
     __m128  f;
     __m128i i;
 
-    VSNRAY_FORCE_INLINE basic_mask() = default;
-    VSNRAY_FORCE_INLINE basic_mask(__m128 m);
-    VSNRAY_FORCE_INLINE basic_mask(__m128i m);
-    VSNRAY_FORCE_INLINE basic_mask(bool x, bool y, bool z, bool w);
-    VSNRAY_FORCE_INLINE basic_mask(bool const v[4]);
-    VSNRAY_FORCE_INLINE basic_mask(bool b);
-    VSNRAY_FORCE_INLINE basic_mask(basic_float<__m128> const& m);
+    basic_mask() = default;
+    basic_mask(__m128 m);
+    basic_mask(__m128i m);
+    basic_mask(bool x, bool y, bool z, bool w);
+    basic_mask(bool const v[4]);
+    basic_mask(bool b);
+    basic_mask(basic_float<__m128> const& m);
 
-    VSNRAY_FORCE_INLINE operator basic_float<__m128>() const;
+    operator basic_float<__m128>() const;
 };
 
 } // simd
