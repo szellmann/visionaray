8d5279ef4f2ca82478f9d63d835831361fea614b
Stefan Zellmann <info@szellmann.de>
2016-07-25 09:01:17 +0200

    Make array_ref compatible with CUDA

--- include/visionaray/array_ref.h
+++ include/visionaray/array_ref.h
@@ -11,6 +11,8 @@
 #include <type_traits>
 #include <vector>
 
+#include "detail/macros.h"
+
 namespace visionaray
 {
 
@@ -42,14 +44,14 @@ public:
 public:
 
     // Construct an empty array_ref.
-    array_ref()
+    VSNRAY_FUNC array_ref()
         : data_(nullptr)
         , len_(0)
     {
     }
 
     // Construct a array_ref from a pointer and a length.
-    array_ref(pointer data, size_t len)
+    VSNRAY_FUNC array_ref(pointer data, size_t len)
         : data_(data)
         , len_(len)
     {
@@ -57,105 +59,105 @@ public:
     }
 
     // Construct from two iterators
-    array_ref(iterator Begin, iterator End)
+    VSNRAY_FUNC array_ref(iterator Begin, iterator End)
         : array_ref(Begin, static_cast<size_t>(End - Begin))
     {
         assert((Begin ? Begin <= End : !End) && "invalid iterators");
     }
 
     // Returns a pointer to the start of the array.
-    pointer data() const
+    VSNRAY_FUNC pointer data() const
     {
         return data_;
     }
 
     // Returns the length of the array.
-    size_t size() const
+    VSNRAY_FUNC size_t size() const
     {
         return len_;
     }
 
     // Returns whether this array is null or empty.
-    bool empty() const
+    VSNRAY_FUNC bool empty() const
     {
         return size() == 0;
     }
 
     // Returns an iterator to the first element of the array.
-    iterator begin() const
+    VSNRAY_FUNC iterator begin() const
     {
         return data();
     }
 
     // Returns an iterator to one element past the last element of the array.
-    iterator end() const
+    VSNRAY_FUNC iterator end() const
     {
         return data() + size();
     }
 
     // Array access.
-    reference operator[](size_t Index) const
+    VSNRAY_FUNC reference operator[](size_t Index) const
     {
         assert(Index < size() && "index out of range");
         return data()[Index];
     }
 
     // Returns the first element of the array.
-    reference front() const
+    VSNRAY_FUNC reference front() const
     {
         assert(!empty() && "index out of range");
         return data()[0];
     }
 
     // Returns the last element of the array.
-    reference back() const
+    VSNRAY_FUNC reference back() const
     {
         assert(!empty() && "index out of range");
         return data()[size() - 1];
     }
 
     // Returns the first N elements of the array.
-    array_ref front(size_t N) const
+    VSNRAY_FUNC array_ref front(size_t N) const
     {
         N = std::min(N, size());
         return { data(), N };
     }
 
     // Removes the first N elements from the array.
-    array_ref drop_front(size_t N) const
+    VSNRAY_FUNC array_ref drop_front(size_t N) const
     {
         N = std::min(N, size());
         return { data() + N, size() - N };
     }
 
     // Returns the last N elements of the array.
-    array_ref back(size_t N) const
+    VSNRAY_FUNC array_ref back(size_t N) const
     {
         N = std::min(N, size());
         return { data() + (size() - N), N };
     }
 
     // Removes the last N elements from the array.
-    array_ref drop_back(size_t N) const
+    VSNRAY_FUNC array_ref drop_back(size_t N) const
     {
         N = std::min(N, size());
         return { data(), size() - N };
     }
 
     // Returns the subarray [First, Last).
-    array_ref slice(size_t First, size_t Last = npos) const
+    VSNRAY_FUNC array_ref slice(size_t First, size_t Last = npos) const
     {
         return front(Last).drop_front(First);
     }
 
     // Returns whether this array is equal to another.
-    bool equals(array_ref rhs) const
+    VSNRAY_FUNC bool equals(array_ref rhs) const
     {
         return size() == rhs.size() && std::equal(begin(), end(), rhs.begin());
     }
 
     // Lexicographically compare this array with another.
-    bool less(array_ref rhs) const
+    VSNRAY_FUNC bool less(array_ref rhs) const
     {
         return std::lexicographical_compare(begin(), end(), rhs.begin(), rhs.end());
     }
@@ -184,36 +186,42 @@ using const_array_ref = array_ref<typename std::add_const<T>::type>;
 //
 
 template <class T>
+VSNRAY_FUNC
 inline bool operator==(array_ref<T> lhs, array_ref<T> rhs)
 {
     return lhs.equals(rhs);
 }
 
 template <class T>
+VSNRAY_FUNC
 inline bool operator!=(array_ref<T> lhs, array_ref<T> rhs)
 {
     return !(lhs == rhs);
 }
 
 template <class T>
+VSNRAY_FUNC
 inline bool operator<(array_ref<T> lhs, array_ref<T> rhs)
 {
     return lhs.less(rhs);
 }
 
 template <class T>
+VSNRAY_FUNC
 inline bool operator<=(array_ref<T> lhs, array_ref<T> rhs)
 {
     return !(rhs < lhs);
 }
 
 template <class T>
+VSNRAY_FUNC
 inline bool operator>(array_ref<T> lhs, array_ref<T> rhs)
 {
     return rhs < lhs;
 }
 
 template <class T>
+VSNRAY_FUNC
 inline bool operator>=(array_ref<T> lhs, array_ref<T> rhs)
 {
     return !(lhs < rhs);
