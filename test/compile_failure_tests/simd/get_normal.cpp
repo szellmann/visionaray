661817e98ff7133b741b97c2361454574c073501
Stefan Zellmann <info@szellmann.de>
2016-08-16 23:07:13 +0200

    Adapt to API changes

--- test/compile_failure_tests/simd/get_normal.cpp
+++ test/compile_failure_tests/simd/get_normal.cpp
@@ -14,53 +14,53 @@ int main()
 
     vector<3, float>* normals;
     hit_record<basic_ray<simd::float4>, primitive<unsigned>> hr;
-    vector<3, simd::float4> n = get_normal(normals, hr, basic_triangle<3, float, unsigned>{}, per_face_binding{});
+    vector<3, simd::float4> n = get_normal(normals, hr, basic_triangle<3, float, unsigned>{}, normals_per_face_binding{});
 
 #elif defined GET_NORMAL_TRIANGLE_PERFACE_FLOAT8
 
 #if VSNRAY_SIMD_ISA >= VSNRAY_SIMD_ISA_AVX
     vector<3, float>* normals;
     hit_record<basic_ray<simd::float8>, primitive<unsigned>> hr;
-    vector<3, simd::float8> n = get_normal(normals, hr, basic_triangle<3, float, unsigned>{}, per_face_binding{});
+    vector<3, simd::float8> n = get_normal(normals, hr, basic_triangle<3, float, unsigned>{}, normals_per_face_binding{});
 #endif
 
 #elif defined GET_NORMAL_TRIANGLE_PERFACE_INT4
 
     vector<3, float>* normals;
     hit_record<basic_ray<simd::int4>, primitive<unsigned>> hr;
-    auto n = get_normal(normals, hr, basic_triangle<3, float, unsigned>{}, per_face_binding{});
+    auto n = get_normal(normals, hr, basic_triangle<3, float, unsigned>{}, normals_per_face_binding{});
 
 #elif defined GET_NORMAL_TRIANGLE_PERFACE_MASK4
 
     vector<3, float>* normals;
     hit_record<basic_ray<simd::mask4>, primitive<unsigned>> hr;
-    auto n = get_normal(normals, hr, basic_triangle<3, float, unsigned>{}, per_face_binding{});
+    auto n = get_normal(normals, hr, basic_triangle<3, float, unsigned>{}, normals_per_face_binding{});
 
 #elif defined GET_NORMAL_TRIANGLE_PERVERTEX_FLOAT4
 
     vector<3, float>* normals;
     hit_record<basic_ray<simd::float4>, primitive<unsigned>> hr;
-    vector<3, simd::float4> n = get_shading_normal(normals, hr, basic_triangle<3, float, unsigned>{}, per_vertex_binding{});
+    vector<3, simd::float4> n = get_shading_normal(normals, hr, basic_triangle<3, float, unsigned>{}, normals_per_vertex_binding{});
 
 #elif defined GET_NORMAL_TRIANGLE_PERVERTEX_FLOAT8
 
 #if VSNRAY_SIMD_ISA >= VSNRAY_SIMD_ISA_AVX
     vector<3, float>* normals;
     hit_record<basic_ray<simd::float8>, primitive<unsigned>> hr;
-    vector<3, simd::float8> n = get_shading_normal(normals, hr, basic_triangle<3, float, unsigned>{}, per_vertex_binding{});
+    vector<3, simd::float8> n = get_shading_normal(normals, hr, basic_triangle<3, float, unsigned>{}, normals_per_vertex_binding{});
 #endif
 
 #elif defined GET_NORMAL_TRIANGLE_PERVERTEX_INT4
 
     vector<3, float>* normals;
     hit_record<basic_ray<simd::int4>, primitive<unsigned>> hr;
-    auto n = get_shading_normal(normals, hr, basic_triangle<3, float, unsigned>{}, per_vertex_binding{});
+    auto n = get_shading_normal(normals, hr, basic_triangle<3, float, unsigned>{}, normals_per_vertex_binding{});
 
 #elif defined GET_NORMAL_TRIANGLE_PERVERTEX_MASK4
 
     vector<3, float>* normals;
     hit_record<basic_ray<simd::mask4>, primitive<unsigned>> hr;
-    auto n = get_shading_normal(normals, hr, basic_triangle<3, float, unsigned>{}, per_vertex_binding{});
+    auto n = get_shading_normal(normals, hr, basic_triangle<3, float, unsigned>{}, normals_per_vertex_binding{});
 
 #endif
 
