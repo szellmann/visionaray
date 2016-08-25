6bfae10c78dccd029dab76b51029d0f081ba8f6f
Stefan Zellmann <info@szellmann.de>
2016-08-25 18:31:26 +0200

    Add a unit test for minmax_element()

--- test/unittests/math/vector.cpp
+++ test/unittests/math/vector.cpp
@@ -82,19 +82,25 @@ TEST(Vector, MinMaxElement)
     // FPU
 
     vector<3, float> vf(1.0f, 2.0f, 3.0f);
-    auto minef = min_element(vf);
-    auto maxef = max_element(vf);
+    auto minef    = min_element(vf);
+    auto maxef    = max_element(vf);
+    auto minmaxef = minmax_element(vf);
     EXPECT_FLOAT_EQ(minef, 1.0f);
     EXPECT_FLOAT_EQ(maxef, 3.0f);
+    EXPECT_FLOAT_EQ(minmaxef.x, minef);
+    EXPECT_FLOAT_EQ(minmaxef.y, maxef);
 
 
     // SSE
 
     vector<3, simd::float4> v4(1.0f, 2.0f, 3.0f);
-    auto mine4 = min_element(v4);
-    auto maxe4 = max_element(v4);
+    auto mine4    = min_element(v4);
+    auto maxe4    = max_element(v4);
+    auto minmaxe4 = minmax_element(v4);
     EXPECT_TRUE( all(mine4 == simd::float4(1.0f)) );
     EXPECT_TRUE( all(maxe4 == simd::float4(3.0f)) );
+    EXPECT_TRUE( all(minmaxe4.x == mine4) );
+    EXPECT_TRUE( all(minmaxe4.y == maxe4) );
 
 
 #if VSNRAY_SIMD_ISA >= VSNRAY_SIMD_ISA_AVX
@@ -102,10 +108,13 @@ TEST(Vector, MinMaxElement)
     // AVX
 
     vector<3, simd::float8> v8(1.0f, 2.0f, 3.0f);
-    auto mine8 = min_element(v8);
-    auto maxe8 = max_element(v8);
+    auto mine8    = min_element(v8);
+    auto maxe8    = max_element(v8);
+    auto minmaxe8 = minmax_element(v8);
     EXPECT_TRUE( all(mine8 == simd::float8(1.0f)) );
     EXPECT_TRUE( all(maxe8 == simd::float8(3.0f)) );
+    EXPECT_TRUE( all(minmaxe8.x == mine8) );
+    EXPECT_TRUE( all(minmaxe8.y == maxe8) );
 
 #endif // VSNRAY_SIMD_ISA >= VSNRAY_SIMD_ISA_AVX
 }
