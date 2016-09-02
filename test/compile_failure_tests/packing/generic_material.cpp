a3036bcc33afb21596482100835da97e66c4ae9e
Stefan Zellmann <info@szellmann.de>
2016-09-02 10:28:43 +0200

    Adapt tests to recent changes

--- test/compile_failure_tests/packing/generic_material.cpp
+++ test/compile_failure_tests/packing/generic_material.cpp
@@ -16,13 +16,13 @@ int main()
 #if defined GENMAT_PACK_FLOAT4
 
     std::array<generic_material<matte<float>,emissive<float>,mirror<float>>, 4> gm_array;
-    simd::generic_material4<matte<float>,emissive<float>,mirror<float>> gm = simd::pack(gm_array);
+    simd::generic_material<4, matte<float>,emissive<float>,mirror<float>> gm = simd::pack(gm_array);
 
 #elif defined GENMAT_PACK_FLOAT8
 
 #if VSNRAY_SIMD_ISA >= VSNRAY_SIMD_ISA_AVX
     std::array<generic_material<matte<float>,emissive<float>,mirror<float>>, 8> gm_array;
-    simd::generic_material8<matte<float>,emissive<float>,mirror<float>> gm = simd::pack(gm_array);
+    simd::generic_material<8, matte<float>,emissive<float>,mirror<float>> gm = simd::pack(gm_array);
 #endif
 
 #elif defined GENMAT_PACK_ILLEGAL_LENGTH_1
@@ -42,24 +42,24 @@ int main()
 
 #elif defined GENMAT_UNPACK_FLOAT4
 
-    simd::generic_material4<matte<float>,emissive<float>,mirror<float>> gm({});
+    simd::generic_material<4, matte<float>,emissive<float>,mirror<float>> gm({});
     std::array<generic_material<matte<float>,emissive<float>,mirror<float>>, 4> gm_array = simd::unpack(gm);
 
 #elif defined GENMAT_UNPACK_FLOAT8
 
 #if VSNRAY_SIMD_ISA >= VSNRAY_SIMD_ISA_AVX
-    simd::generic_material8<matte<float>,emissive<float>,mirror<float>> gm({});
+    simd::generic_material<8, matte<float>,emissive<float>,mirror<float>> gm({});
     std::array<generic_material<matte<float>,emissive<float>,mirror<float>>, 8> gm_array = simd::unpack(gm);
 #endif
 
 #elif defined GENMAT_UNPACK_ILLEGAL_LENGTH
 
-    simd::generic_material4<matte<float>,emissive<float>,mirror<float>> gm({});
+    simd::generic_material<4, matte<float>,emissive<float>,mirror<float>> gm({});
     std::array<generic_material<matte<float>,emissive<float>,mirror<float>>, 8> gm_array = simd::unpack(gm);
 
 #elif defined GENMAT_UNPACK_ILLEGAL_INTEGRAL
 
-    simd::generic_material4<float> gm({});
+    simd::generic_material<4, float> gm({});
     auto gm_array = simd::unpack(gm);
 
 #elif defined GENMAT_UNPACK_SINGLE
