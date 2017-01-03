1f32f69225047670165c80efbf1110eba3b5f8c0
Stefan Zellmann <info@szellmann.de>
2017-01-03 10:48:38 +0100

    No GL_CLAMP in modern OpenGL

--- src/visionaray/gl/compositing.cpp
+++ src/visionaray/gl/compositing.cpp
@@ -131,8 +131,8 @@ void depth_compositor::impl::set_texture_params() const
 {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 }
 
 
