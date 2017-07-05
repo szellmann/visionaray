3462de7a17b8a98ca21a6a00d093148ac556b8ec
Stefan Zellmann <info@szellmann.de>
2017-07-05 12:01:29 +0200

    Compile fix

--- src/examples/nanort/main.cpp
+++ src/examples/nanort/main.cpp
@@ -19,6 +19,7 @@
 #include <visionaray/cpu_buffer_rt.h>
 #include <visionaray/get_normal.h>
 #include <visionaray/random_sampler.h>
+#include <visionaray/sampling.h>
 #include <visionaray/scheduler.h>
 #include <visionaray/traverse.h>
 
