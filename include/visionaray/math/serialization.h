38419e6d14123eb293544455af330cbeee89497b
Stefan Zellmann <info@szellmann.de>
2017-07-11 09:11:24 +0200

    Serialization function for 3x3 matrices

--- include/visionaray/math/serialization.h
+++ include/visionaray/math/serialization.h
@@ -54,6 +54,15 @@ inline void serialize(A& a, MATH_NAMESPACE::vector<4, T>& v, unsigned /* version
 
 
 template <typename A, typename T>
+inline void serialize(A& a, MATH_NAMESPACE::matrix<3, 3, T>& m, unsigned /* version */ )
+{
+    a & m.col0;
+    a & m.col1;
+    a & m.col2;
+}
+
+
+template <typename A, typename T>
 inline void serialize(A& a, MATH_NAMESPACE::matrix<4, 4, T>& m, unsigned /* version */ )
 {
     a & m.col0;
