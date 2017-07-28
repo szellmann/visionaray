1aba56845c759d1e06f69d658f79c15c53b41297
Stefan Zellmann <info@szellmann.de>
2017-07-28 11:11:55 +0200

    HDR temp

--- src/common/hdr_image.cpp
+++ src/common/hdr_image.cpp
@@ -9,6 +9,8 @@
 #include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
 
+#include <visionaray/math/vector.h>
+
 #include "hdr_image.h"
 
 namespace visionaray
@@ -17,6 +19,12 @@ namespace visionaray
 bool hdr_image::load(std::string const& filename)
 {
     std::ifstream file(filename);
+
+    if (!file.good())
+    {
+        std::cerr << "Cannot open HDR file: " << filename << '\n';
+        return false;
+    }
 
     // parse information header ---------------------------
 
@@ -75,7 +83,8 @@ bool hdr_image::load(std::string const& filename)
 
     if (res.size() != 4)
     {
-        std::cout << "Error: invalid resolution string in HDR file\n";
+        std::cerr << "Error: invalid resolution string in HDR file\n";
+        return false;
     }
 
     if (res[0] == "-Y" && res[2] == "+X")
@@ -85,27 +94,55 @@ bool hdr_image::load(std::string const& filename)
     }
     else
     {
-        std::cout << "Error: unsupported resolution string in HDR file\n";
+        std::cerr << "Error: unsupported resolution string in HDR file\n";
+        return false;
     }
 
     // scanlines ------------------------------------------
 
-    //while (std::getline(file, line))
+    std::vector<vec4> rgbe_line(width_);
+
     for (size_t h = 0; h < height_; ++h)
     {
         std::getline(file, line);
         auto bytes = reinterpret_cast<uint8_t const*>(line.c_str());
 
-        for (size_t w = 0; w < width_; ++w)
-        {
-            uint8_t r = bytes[w * 4];
-            uint8_t g = bytes[w * 4 + 1];
-            uint8_t b = bytes[w * 4 + 2];
-            uint8_t e = bytes[w * 4 + 3];
+        // Read the first four bytes of each scaline
+        // Radiance's new RLE: 0x2 0x2 means RLE
+        uint8_t r = *bytes++;
+        uint8_t g = *bytes++;
+        uint8_t b = *bytes++;
+        uint8_t e = *bytes++;
 
-            size_t rl = 1;
-            if (r == 2 && g == 2)
+        if (r == 2 && g == 2)
+        {
+            // Component-wise RLE
+            for (size_t c = 0; c < 4; ++c)
             {
+                size_t x = 0;
+                while (x < width_)
+                {
+                    uint8_t runlength = *bytes++;
+
+                    if (runlength <= 128) // no run!
+                    {
+                        for (size_t i = 0; i < runlength; ++i)
+                        {
+                            rgbe_line[i][c] = *bytes++;
+                        }
+                    }
+                    else
+                    {
+                        runlength -= 128;
+
+                        for (size_t i = 0; i < runlength; ++i)
+                        {
+                        }
+                    }
+
+                    x += runlength;
+                }
+                std::cout << x << '\n';
             }
         }
     }
