a8289a603c81b36a9745eec6d4ea3e46491028d5
Stefan Zellmann <info@szellmann.de>
2017-11-28 07:06:52 -0800

    Have only one random_sampler for simd types

--- include/visionaray/random_sampler.h
+++ include/visionaray/random_sampler.h
@@ -75,7 +75,7 @@ VSNRAY_FUNC array<unsigned, simd::num_elements<T>::value> tic(T /* */)
 // random_sampler classes, uses a standard pseudo RNG to generate samples
 //
 
-template <typename T>
+template <typename T, typename = void>
 class random_sampler
 {
 public:
@@ -115,35 +115,38 @@ private:
 
 };
 
-template <>
-class random_sampler<simd::float4>
+template <typename T>
+class random_sampler<T, typename std::enable_if<simd::is_simd_vector<T>::value>::type>
 {
 public:
 
-    using value_type = simd::float4;
+    using value_type = T;
 
 public:
 
     typedef random_sampler<float> sampler_type;
 
-    VSNRAY_FUNC random_sampler(array<unsigned, 4> const& seed)
-        : samplers_({{ seed[0], seed[1], seed[2], seed[3] }}) // TODO!
+    VSNRAY_FUNC random_sampler(array<unsigned, simd::num_elements<value_type>::value> const& seed)
     {
+        for (int i = 0; i < simd::num_elements<value_type>::value; ++i)
+        {
+            samplers_[i] = sampler_type(seed[i]);
+        }
     }
 
-    VSNRAY_FUNC simd::float4 next()
+    VSNRAY_FUNC value_type next()
     {
         simd::aligned_array_t<value_type> arr;
 
-        for (int i = 0; i < simd::num_elements<simd::float4>::value; ++i)
+        for (int i = 0; i < simd::num_elements<value_type>::value; ++i)
         {
             arr[i] = samplers_[i].next();
         }
 
-        return simd::float4(arr);
+        return value_type(arr);
     }
 
-    // TODO: maybe don't have a random_sampler4 at all?
+    // TODO: maybe don't have a random_samplerN at all?
     sampler_type& get_sampler(size_t i)
     {
         return samplers_[i];
@@ -151,91 +154,7 @@ public:
 
 private:
 
-    array<sampler_type, simd::num_elements<simd::float4>::value> samplers_;
-
-};
-
-template <>
-class random_sampler<simd::float8>
-{
-public:
-
-    using value_type = simd::float8;
-
-public:
-
-    typedef random_sampler<float> sampler_type;
-
-    VSNRAY_FUNC random_sampler(array<unsigned, 8> const& seed)
-        : samplers_({{ seed[0], seed[1], seed[2], seed[3],
-                       seed[4], seed[5], seed[6], seed[7] }}) // TODO!
-    {
-    }
-
-    VSNRAY_FUNC simd::float8 next()
-    {
-        simd::aligned_array_t<value_type> arr;
-
-        for (int i = 0; i < simd::num_elements<simd::float8>::value; ++i)
-        {
-            arr[i] = samplers_[i].next();
-        }
-
-        return simd::float8(arr);
-    }
-
-    // TODO: maybe don't have a random_sampler8 at all?
-    sampler_type& get_sampler(size_t i)
-    {
-        return samplers_[i];
-    }
-
-private:
-
-    array<sampler_type, simd::num_elements<simd::float8>::value> samplers_;
-
-};
-
-template <>
-class random_sampler<simd::float16>
-{
-public:
-
-    using value_type = simd::float16;
-
-public:
-
-    typedef random_sampler<float> sampler_type;
-
-    VSNRAY_FUNC random_sampler(array<unsigned, 16> const& seed)
-        : samplers_({{ seed[ 0], seed[ 1], seed[ 2], seed[ 3],
-                       seed[ 4], seed[ 5], seed[ 6], seed[ 7],
-                       seed[ 8], seed[ 9], seed[10], seed[11],
-                       seed[12], seed[13], seed[14], seed[15] }}) // TODO!
-    {
-    }
-
-    VSNRAY_FUNC simd::float16 next()
-    {
-        simd::aligned_array_t<value_type> arr;
-
-        for (int i = 0; i < simd::num_elements<simd::float16>::value; ++i)
-        {
-            arr[i] = samplers_[i].next();
-        }
-
-        return simd::float16(arr);
-    }
-
-    // TODO: maybe don't have a random_sampler16 at all?
-    sampler_type& get_sampler(size_t i)
-    {
-        return samplers_[i];
-    }
-
-private:
-
-    array<sampler_type, simd::num_elements<simd::float16>::value> samplers_;
+    array<sampler_type, simd::num_elements<value_type>::value> samplers_;
 
 };
 
