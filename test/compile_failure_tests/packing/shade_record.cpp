21794ae937a4d0d0a847ce89137545743dfa5eff
Stefan Zellmann <info@szellmann.de>
2018-01-19 13:46:21 -0500

    shade_record no longer stores light source

--- test/compile_failure_tests/packing/shade_record.cpp
+++ test/compile_failure_tests/packing/shade_record.cpp
@@ -4,7 +4,6 @@
 #include <visionaray/math/math.h>
 #include <visionaray/array.h>
 #include <visionaray/shade_record.h>
-#include <visionaray/point_light.h>
 
 using namespace visionaray;
 
@@ -14,29 +13,29 @@ int main()
 
 #if defined SHADE_RECORD_UNPACK_FLOAT4
 
-    shade_record<point_light<float>, simd::float4> sr;
-    array<shade_record<point_light<float>, float>, 4> sr_array = simd::unpack(sr);
+    shade_record<simd::float4> sr;
+    array<shade_record<float>, 4> sr_array = simd::unpack(sr);
 
 #elif defined SHADE_RECORD_UNPACK_FLOAT8
 
 #if VSNRAY_SIMD_ISA_GE(VSNRAY_SIMD_ISA_AVX)
-    shade_record<point_light<float>, simd::float8> sr;
-    array<shade_record<point_light<float>, float>, 8> sr_array = simd::unpack(sr);
+    shade_record<simd::float8> sr;
+    array<shade_record<float>, 8> sr_array = simd::unpack(sr);
 #endif
 
 #elif defined SHADE_RECORD_UNPACK_ILLEGAL_LENGTH
 
-    shade_record<point_light<float>, simd::float4> sr;
-    array<shade_record<point_light<float>, float>, 8> sr_array = simd::unpack(sr);
+    shade_record<simd::float4> sr;
+    array<shade_record<float>, 8> sr_array = simd::unpack(sr);
 
 #elif defined SHADE_RECORD_UNPACK_ILLEGAL_INTEGRAL
 
-    shade_record<point_light<float>, simd::int4> sr;
+    shade_record<simd::int4> sr;
     auto sr_array = simd::unpack(sr);
 
 #elif defined SHADE_RECORD_UNPACK_SINGLE
 
-    shade_record<point_light<float>, float> sr;
+    shade_record<float> sr;
     auto sr_array = simd::unpack(sr);
 
 #endif
