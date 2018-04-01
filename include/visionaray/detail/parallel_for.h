48990d25d16e87e1a4107d2acc23fe92a5b8e6c3
Stefan Zellmann <info@szellmann.de>
2018-04-01 22:02:46 +0200

    2D parallel for implementation

--- include/visionaray/detail/parallel_for.h
+++ include/visionaray/detail/parallel_for.h
@@ -20,7 +20,7 @@ namespace visionaray
 {
 
 //-------------------------------------------------------------------------------------------------
-// Simple range class
+// Simple 1-D range class
 //
 
 template <typename I>
@@ -56,7 +56,61 @@ private:
 
 
 //-------------------------------------------------------------------------------------------------
-// Tiled range class
+// Simple 2-D range class
+//
+
+template <typename I>
+class range2d
+{
+public:
+    static_assert(std::is_integral<I>::value, "Type must be integral.");
+
+    range2d(I rb, I re, I cb, I ce)
+        : row_begin_(rb)
+        , row_end_(re)
+        , col_begin_(cb)
+        , col_end_(ce)
+    {
+    }
+
+    I&        row_begin()           { return row_begin_; }
+    I const&  row_begin()     const { return row_begin_; }
+    I const& crow_begin()     const { return row_begin_; }
+
+    I&        row_end()             { return row_end_; }
+    I const&  row_end()       const { return row_end_; }
+    I const& crow_end()       const { return row_end_; }
+
+    I&        col_begin()           { return col_begin_; }
+    I const&  col_begin()     const { return col_begin_; }
+    I const& ccol_begin()     const { return col_begin_; }
+
+    I&        col_end()             { return col_end_; }
+    I const&  col_end()       const { return col_end_; }
+    I const& ccol_end()       const { return col_end_; }
+
+    I row_length() const
+    {
+        return row_end_ - row_begin_;
+    }
+
+    I col_length() const
+    {
+        return col_end_ - col_begin_;
+    }
+
+private:
+
+    I row_begin_;
+    I row_end_;
+
+    I col_begin_;
+    I col_end_;
+};
+
+
+//-------------------------------------------------------------------------------------------------
+// 1-D tiled range class
 //
 
 template <typename I>
@@ -93,6 +147,70 @@ private:
     I begin_;
     I end_;
     I tile_size_;
+};
+
+
+//-------------------------------------------------------------------------------------------------
+// 2-D tiled range class
+//
+
+template <typename I>
+class tiled_range2d
+{
+public:
+    static_assert(std::is_integral<I>::value, "Type must be integral.");
+
+    tiled_range2d(I rb, I re, I rts, I cb, I ce, I cts)
+        : row_begin_(rb)
+        , row_end_(re)
+        , row_tile_size_(rts)
+        , col_begin_(cb)
+        , col_end_(ce)
+        , col_tile_size_(cts)
+    {
+    }
+
+    I&        row_begin()           { return row_begin_; }
+    I const&  row_begin()     const { return row_begin_; }
+    I const& crow_begin()     const { return row_begin_; }
+
+    I&        row_end()             { return row_end_; }
+    I const&  row_end()       const { return row_end_; }
+    I const& crow_end()       const { return row_end_; }
+
+    I&        row_tile_size()       { return row_tile_size_; }
+    I const&  row_tile_size() const { return row_tile_size_; }
+
+    I&        col_begin()           { return col_begin_; }
+    I const&  col_begin()     const { return col_begin_; }
+    I const& ccol_begin()     const { return col_begin_; }
+
+    I&        col_end()             { return col_end_; }
+    I const&  col_end()       const { return col_end_; }
+    I const& ccol_end()       const { return col_end_; }
+
+    I&        col_tile_size()       { return col_tile_size_; }
+    I const&  col_tile_size() const { return col_tile_size_; }
+
+    I row_length() const
+    {
+        return row_end_ - row_begin_;
+    }
+
+    I col_length() const
+    {
+        return col_end_ - col_begin_;
+    }
+
+private:
+
+    I row_begin_;
+    I row_end_;
+    I row_tile_size_;
+
+    I col_begin_;
+    I col_end_;
+    I col_tile_size_;
 };
 
 
@@ -283,6 +401,26 @@ void parallel_for(thread_pool& pool, tiled_range1d<I> const& range, Func const& 
             func(range1d<I>(first, last));
 
         }, static_cast<long>(num_tiles));
+}
+
+template <typename I, typename Func>
+void parallel_for(thread_pool& pool, tiled_range2d<I> const& range, Func const& func)
+{
+    unsigned tile_width = static_cast<unsigned>(range.row_tile_size());
+    unsigned tile_height = static_cast<unsigned>(range.col_tile_size());
+    unsigned num_tiles_x = div_up(static_cast<unsigned>(range.row_length()), tile_width);
+    unsigned num_tiles_y = div_up(static_cast<unsigned>(range.col_length()), tile_height);
+
+    pool.run([&](long tile_index)
+        {
+            func(range2d<I>(
+                    (tile_index % num_tiles_x) * tile_width,
+                    tile_width,
+                    (tile_index / num_tiles_x) * tile_height,
+                    tile_height
+                    ));
+
+        }, static_cast<long>(num_tiles_x * num_tiles_y));
 }
 
 } // visionaray
