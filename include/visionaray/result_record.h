1727d045b327c24bacf23ae4cdd6de263986c0b2
Stefan Zellmann <info@szellmann.de>
2018-06-30 12:45:57 +0200

    Use C++11 default initializers

--- include/visionaray/result_record.h
+++ include/visionaray/result_record.h
@@ -17,30 +17,17 @@ namespace visionaray
 //
 
 template <typename T>
-class result_record
+struct result_record
 {
-public:
-
     using scalar_type = T;
     using mask_type   = simd::mask_type_t<T>;
     using vec_type    = vector<3, T>;
     using color_type  = vector<4, T>;
 
-public:
-
-    VSNRAY_FUNC result_record()
-        : hit(false)
-        , color(0.0)
-        , depth(0.0)
-        , isect_pos(0.0)
-    {
-    }
-
-    mask_type   hit;
-    color_type  color;
-    scalar_type depth;
-    vec_type    isect_pos;
-
+    mask_type   hit       = false;
+    color_type  color     = color_type(0.0);
+    scalar_type depth     = scalar_type(0.0);
+    vec_type    isect_pos = vec_type(0.0);
 };
 
 } // visionaray
