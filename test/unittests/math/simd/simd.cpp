88aaf70a02a3f102a47248ac74c7ea0b70846cf0
Stefan Zellmann <info@szellmann.de>
2018-07-01 13:32:17 +0200

    Fix unit tests

--- test/unittests/math/simd/simd.cpp
+++ test/unittests/math/simd/simd.cpp
@@ -1246,13 +1246,13 @@ TEST(SIMD, Transposition)
         // shuffle
 
         simd::float8 u(1, 2, 3, 4, 5, 6, 7, 8);
-        simd::float8 v(9, 10, 11, 12, 13, 14, 15, 16);
+        simd::float8 v(9, 10, 11, 12, 13, 14, 15, 16);std::cout << interleave_lo(u, v) << ' ' << interleave_hi(u, v) << '\n';
 
 
         // interleave_xx
 
-        EXPECT_TRUE( all(interleave_lo(u, v)             == simd::float8(1, 9, 2, 10, 3, 11, 4, 12)) );
-        EXPECT_TRUE( all(interleave_hi(u, v)             == simd::float8(5, 13, 6, 14, 7, 15, 8, 16)) );
+        EXPECT_TRUE( all(interleave_lo(u, v)             == simd::float8(1, 9, 2, 10, 5, 13, 6, 14)) );
+        EXPECT_TRUE( all(interleave_hi(u, v)             == simd::float8(3, 11, 4, 12, 7, 15, 8, 16)) );
     }
 }
 
