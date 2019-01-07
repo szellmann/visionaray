c801f1597c9674b38f42232eaabb00cebbedf799
Stefan Zellmann <info@szellmann.de>
2019-01-07 11:29:48 +0100

    Layout

--- include/visionaray/kernels.h
+++ include/visionaray/kernels.h
@@ -170,17 +170,17 @@ auto make_kernel_params(
         vec4 const&         ambient_color   = vec4(0.0)
         )
     -> kernel_params<
-            normals_per_vertex_binding,
-            unspecified_binding,
-            Primitives,
-            vector<3, typename scalar_type<typename std::iterator_traits<Primitives>::value_type>::type>*,
-            vector<2, typename scalar_type<typename std::iterator_traits<Primitives>::value_type>::type>*,
-            Materials,
-            vector<3, typename scalar_type<typename std::iterator_traits<Primitives>::value_type>::type>*,
-            texture<unorm<8>, 2>*,
-            Lights,
-            vec4
-            >
+        normals_per_vertex_binding,
+        unspecified_binding,
+        Primitives,
+        vector<3, typename scalar_type<typename std::iterator_traits<Primitives>::value_type>::type>*,
+        vector<2, typename scalar_type<typename std::iterator_traits<Primitives>::value_type>::type>*,
+        Materials,
+        vector<3, typename scalar_type<typename std::iterator_traits<Primitives>::value_type>::type>*,
+        texture<unorm<8>, 2>*,
+        Lights,
+        vec4
+        >
 {
     return {
         { begin, end },
