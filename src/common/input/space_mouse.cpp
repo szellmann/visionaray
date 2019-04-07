6dbbbaf35fc8b2d1e678365dc917976a86b52d54
Stefan Zellmann <info@szellmann.de>
2019-04-07 12:05:47 +0200

    Compile fix

--- src/common/input/space_mouse.cpp
+++ src/common/input/space_mouse.cpp
@@ -3,14 +3,14 @@
 
 #include <common/config.h>
 
+#if VSNRAY_COMMON_HAVE_3DCONNEXIONCLIENT
+
 #include <cassert>
 
 #include <3DConnexionClient/ConnexionClient.h>
 #include <3DConnexionClient/ConnexionClientAPI.h>
 
 #include "space_mouse.h"
-
-#if VSNRAY_COMMON_HAVE_3DCONNEXIONCLIENT
 
 // Import weak so we can later check if the
 // driver is loaded by checking for NULL
