3c06d37be2ba35fb3c9d197ed7541e67767a0308
Stefan Zellmann <info@szellmann.de>
2019-09-06 11:30:40 +0200

    Enable device to host copy for BVHs

--- include/visionaray/bvh.h
+++ include/visionaray/bvh.h
@@ -596,6 +596,20 @@ private:
             cudaMemcpyHostToDevice
             );
     }
+
+    template <typename DstVector, typename T>
+    void copy(DstVector& dst, thrust::device_vector<T> const& src)
+    {
+        // Trivial copy. See copy(device_vector, host_vector)
+        dst.resize(src.size());
+
+        cudaMemcpy(
+            dst.data(),
+            thrust::raw_pointer_cast(src.data()),
+            sizeof(T) * src.size(),
+            cudaMemcpyDeviceToHost
+            );
+    }
 #endif
 };
 
