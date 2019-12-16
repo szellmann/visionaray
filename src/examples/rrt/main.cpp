ca37f990fb5f59d868d2bd4775409745dd77b708
Stefan Zellmann <info@szellmann.de>
2019-12-16 19:54:31 +0100

    Add Le

--- src/examples/rrt/main.cpp
+++ src/examples/rrt/main.cpp
@@ -31,17 +31,128 @@ using viewer_type   = viewer_glut;
 
 
 //-------------------------------------------------------------------------------------------------
-// Texture data
+// Procedural volumes
+// Using the same as in "Ray Tracing Inhomogeneous Volumes" by Matthias Raab (Ray Tracing Gems 1)
 //
 
-// post-classification transfer function
-VSNRAY_ALIGN(32) static const vec4 tfdata[5 * 4] = {
-        { 1.0f, 0.0f, 0.0f, 1.0f   },
-        { 1.0f, 0.0f, 0.0f, 0.2f   },
-        { 1.0f, 0.0f, 0.0f, 0.2f   },
-        { 1.0f, 0.0f, 0.0f, 0.2f   },
-        { 1.0f, 1.0f, 1.0f, 0.002f }
-        };
+template <int Mode>
+struct volume
+{
+    // Extinction (absorption+scattering)
+    float sigma_t(vec3 const& pos) const
+    {
+        if (Mode == 0) // spiral
+        {
+            const float r = 0.5f * (0.5f - fabsf (pos.y));
+            const float a = (float)(M_PI * 8.0) * pos.y;
+            const float dx = (cosf(a) * r - pos.x) * 2.0f;
+            const float dy = (sinf(a) * r - pos.z) * 2.0f;
+            return powf (fmaxf((1.0f - dx * dx - dy * dy), 0.0f), 8.0f) * sigma_();
+        }
+        else // menger sponge
+        {
+            vec3 ppos = pos + vec3(0.5f, 0.5f, 0.5f);
+            const unsigned int steps = 3;
+            for (unsigned int i = 0; i < steps; ++i)
+            {
+                ppos *= 3.0f;
+
+                const int s = ((int)ppos.x & 1) + ((int)ppos.y & 1) + ((int)ppos.z & 1);
+
+                if (s >= 2)
+                {
+                    return 0.0f;
+                }
+            }
+            return sigma_();
+        }
+    }
+
+    float sigma_a(vec3 const& pos) const
+    {
+        // Model extinction as 1:1 absorption and out-scattering
+        return sigma_t(pos) / 2.0f;
+    }
+
+    // Majorant
+    float sigma_() const
+    {
+        return 50.0f;
+    }
+
+    // Emission
+    vec3 Le(vec3 const& pos) const
+    {
+        if (pos.x > 0.16f && pos.y > 0.16f && pos.z > 0.16f)
+         //&& pos.x < 0.33f && pos.y < 0.33f && pos.z < 0.33f)
+        {
+            return vec3(4.0f, 2.0f, 1.5f);
+            //return vec3(120.0f, 60.0f, 45.0f);
+        }
+        else
+        {
+            return vec3(0.0f);
+        }
+    }
+};
+
+
+//-------------------------------------------------------------------------------------------------
+//
+//
+
+enum collision_type
+{
+    Scattering,
+    Emission,
+    Boundary,
+};
+
+namespace delta_tracking
+{
+
+template <int Mode>
+collision_type sample_interaction(ray& r, ::volume<Mode> const& vol, vec3& Le, float& Tr,float d, random_generator<float>& gen)
+{
+    collision_type result;
+
+    float t = 0.0f;
+    vec3 pos;
+
+    Le = vec3(0.0f);
+
+    while (true)
+    {
+        t -= log(1.0f - gen.next()) / vol.sigma_();
+
+        pos = r.ori + r.dir * t;
+        if (t >= d)
+        {
+            result = Boundary;
+            break;
+        }
+
+        float u2 = gen.next();
+        if (u2 < vol.sigma_a(pos) / vol.sigma_t(pos))
+        {
+            Le = vol.Le(pos);
+            result = Emission;
+            break;
+        }
+        else if (vol.sigma_t(pos) >= u2 * vol.sigma_())
+        {
+            result = Scattering;
+            break;
+        }
+    }
+
+    Tr = result == Boundary ? 1.0f : 0.0f;
+    r.ori = pos;
+
+    return result;
+}
+
+}
 
 
 //-------------------------------------------------------------------------------------------------
@@ -54,60 +165,9 @@ struct renderer : viewer_type
 
     renderer()
         : viewer_type(512, 512, "Visionaray Residual Ratio Tracking Example")
-        //, bbox({ -1.0f, -1.0f, -1.0f }, { 1.0f, 1.0f, 1.0f })
         , bbox({ -0.5f, -0.5f, -0.5f }, { 0.5f, 0.5f, 0.5f })
         , host_sched(8)
-        , volume({256, 256, 256})
-        , transfunc(5)
     {
-        size_t width  = volume.width();
-        size_t height = volume.height();
-        size_t depth  = volume.depth();
-
-        voldata.resize(width * height * depth);
-
-        for (size_t z = 0; z < depth; ++z)
-        {
-            for (size_t y = 0; y < height; ++y)
-            {
-                for (size_t x = 0; x < width; ++x)
-                {
-                    size_t index = z * width * height + y * width + x;
-
-                    auto xx = (float)x / width  * 4.0f - 2.0f;
-                    auto yy = (float)y / height * 4.0f - 2.0f;
-                    auto zz = (float)z / depth  * 4.0f - 2.0f;
-
-                    voldata[index] = pow(xx * xx + (9.0f / 4.0f) * (yy * yy) + zz * zz - 1, 3)
-                           - (xx * xx) * (zz * zz * zz) - (9.0f / 80.0f) * (yy * yy) * (zz * zz * zz);
-                }
-            }
-        }
-
-        volume.reset(voldata.data());
-        volume.set_filter_mode(Linear);
-        volume.set_address_mode(Clamp);
-
-        transfunc.reset(tfdata);
-        transfunc.set_filter_mode(Linear);
-        transfunc.set_address_mode(Clamp);
-
-
-        // https://graphics.pixar.com/library/ProductionVolumeRendering/paper.pdf
-        // To homogenize the overall volume, we choose σn (x) in such a way that the sum
-        // of all coefficients, the free-path coefficient σ ̄, becomes constant: σ ̄
-        // =σa(x)+σs(x)+σn(x)=σt(x)+σn(x) (20) A consequence of the need to be constant is
-        // that σ ̄ is equal or greater to the maximum of σt (x) (sometimes formulated as
-        // being a majorant of σt (x)): and we can easily calculate σn (x ) from σ ̄ ≥ σt
-        // (x) (21) σn (x ) = σ ̄ − σt (x).
-
-        // So the majorant is just the maximum value (?)
-        majorant = -FLT_MAX;
-
-        for (size_t i = 0; i < voldata.size(); ++i)
-        {
-            majorant = max(majorant, voldata[i]);
-        }
     }
 
     aabb                                        bbox;
@@ -115,19 +175,9 @@ struct renderer : viewer_type
     cpu_buffer_rt<PF_RGBA32F, PF_UNSPECIFIED>   host_rt;
     tiled_sched<host_ray_type>                  host_sched;
 
-
-    // Volume data
-    aligned_vector<float> voldata;
-
-
-    // texture references
-
-    texture_ref<float, 3>                       volume;
-    texture_ref<vec4, 1>                        transfunc;
-
-    float majorant;
-
     unsigned frame_num = 0;
+
+    ::volume<1> vol;
 
 protected:
 
@@ -157,63 +207,7 @@ void renderer::on_display()
             host_rt
             );
 
-    float mu_ = 50.0f;
-    //float mu_ = majorant;
     float albedo = 0.8f;
-
-    auto mu = [&](vec3 const& pos)
-    {
-#if 0
-        const float r = 0.5f * (0.5f - fabsf (pos.y));
-        const float a = (float)(M_PI * 8.0) * pos.y;
-        const float dx = (cosf(a) * r - pos.x) * 2.0f;
-        const float dy = (sinf(a) * r - pos.z) * 2.0f;
-        return powf (fmaxf((1.0f - dx * dx - dy * dy), 0.0f), 8.0f) * mu_;
-#elif 1
-        vec3 ppos = pos + vec3(0.5f, 0.5f, 0.5f);
-        const unsigned int steps = 3;
-        for (unsigned int i = 0; i < steps; ++i)
-        {
-            ppos *= 3.0f;
-
-            const int s = ((int)ppos.x & 1) + ((int)ppos.y & 1) + ((int)ppos.z & 1);
-
-            if (s >= 2)
-            {
-                return 0.0f;
-            }
-        }
-        return mu_;
-#else
-        vec3 tex_coord(
-                ( pos.x + 1.0f ) / 2.0f,
-                (-pos.y + 1.0f ) / 2.0f,
-                (-pos.z + 1.0f ) / 2.0f
-                );
-        return tex3D(volume, tex_coord);
-#endif
-    };
-
-    auto sample_interaction = [&](ray& r, float d, random_generator<float>& gen)
-    {
-        float t = 0.0f;
-        vec3 pos;
-
-        do
-        {
-            t -= log(1.0f - gen.next()) / mu_;
-
-            pos = r.ori + r.dir * t;
-            if (t >= d)
-            {
-                return false;
-            }
-        }
-        while (mu(pos) < gen.next() * mu_);
-
-        r.ori = pos;
-        return true;
-    };
 
     henyey_greenstein<float> f;
     f.g = 0.0f; // isotropic
@@ -235,8 +229,18 @@ void renderer::on_display()
 
             unsigned bounce = 0;
 
-            while (sample_interaction(r, hit_rec.tfar, gen))
+            while (true)
             {
+                vec3 Le;
+                float Tr;
+                collision_type coll = delta_tracking::sample_interaction(r, vol, Le, Tr, hit_rec.tfar, gen);
+
+                if (coll == Boundary)
+                {
+                    // Outside of the volume
+                    break;
+                }
+
                 // Is the path length exceeded?
                 if (bounce++ >= 1024)
                 {
@@ -256,6 +260,8 @@ void renderer::on_display()
                     }
                     throughput /= prob;
                 }
+
+                throughput += Le;
 
                 // Sample phase function
                 vec3 scatter_dir;
