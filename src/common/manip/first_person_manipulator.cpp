9d32ea9a9c54fb511c8885f247822001ae8bbac1
Stefan Zellmann <info@szellmann.de>
2020-03-28 20:20:43 +0100

    Not used

--- src/common/manip/first_person_manipulator.cpp
+++ src/common/manip/first_person_manipulator.cpp
@@ -1,11 +1,6 @@
 // This file is distributed under the MIT license.
 // See the LICENSE file for details.
 
-#ifndef NDEBUG
-#include <iomanip>
-#include <iostream>
-#endif
-
 #include <visionaray/math/axis.h>
 #include <visionaray/math/forward.h>
 #include <visionaray/math/matrix.h>
