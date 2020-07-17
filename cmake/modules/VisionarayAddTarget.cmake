d2b0b7bd005412f6c729377b0b83dba85b2d967e
Stefan Zellmann <info@szellmann.de>
2020-07-18 01:55:44 +0200

    Set warnings per target

--- cmake/modules/VisionarayAddTarget.cmake
+++ cmake/modules/VisionarayAddTarget.cmake
@@ -5,6 +5,49 @@ macro(visionaray_link_libraries)
     set(__VSNRAY_LINK_LIBRARIES ${__VSNRAY_LINK_LIBRARIES} ${ARGN})
 endmacro()
 
+function(visionaray_target_set_warnings target)
+    if(MSVC)
+
+    elseif(__COMPILER_PGI)
+
+    else() # GNU, Clang, etc.
+
+        #------------------------------------------------------
+        # Enable warnings
+        #
+
+        target_compile_options(
+            ${target} PRIVATE
+            -Wmissing-braces
+            -Wsign-compare
+            -Wwrite-strings
+            -Woverloaded-virtual
+            -Wundef
+        )
+
+        if(VSNRAY_ENABLE_WARNINGS)
+            target_compile_options(${target} PRIVATE -Wall -Wextra)
+
+            if(VSNRAY_ENABLE_PEDANTIC)
+                target_compile_options(${target} PRIVATE -pedantic)
+            endif()
+        endif()
+
+
+        #------------------------------------------------------
+        # Selectively disable warnings
+        #
+
+        if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+            if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 6.0)
+                # Disable warnings like:
+                # warning: ignoring attributes on template argument __m128i
+                target_compile_options(${target} PUBLIC -Wno-ignored-attributes)
+            endif()
+        endif()
+    endif()
+endfunction()
+
 function(visionaray_cuda_compile outfiles)
     if(NOT CUDA_FOUND OR NOT VSNRAY_ENABLE_CUDA)
         return()
@@ -31,6 +74,7 @@ endfunction()
 
 function(visionaray_add_executable name)
     add_executable(${name} ${ARGN})
+    visionaray_target_set_warnings(${name})
     target_link_libraries(${name} ${__VSNRAY_LINK_LIBRARIES})
 
     if(VSNRAY_MACOSX_BUNDLE)
@@ -44,6 +88,7 @@ function(visionaray_add_cuda_executable name)
     endif()
 
     cuda_add_executable(${name} ${ARGN})
+    #visionaray_target_set_warnings(${name})
     target_link_libraries(${name} ${__VSNRAY_LINK_LIBRARIES})
 
     if(VSNRAY_MACOSX_BUNDLE)
@@ -53,5 +98,6 @@ endfunction()
 
 function(visionaray_add_library name)
     add_library(${name} ${ARGN})
+    visionaray_target_set_warnings(${name})
     target_link_libraries(${name} ${__VSNRAY_LINK_LIBRARIES})
 endfunction()
