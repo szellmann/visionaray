d14c21926b3feedf0709f38aa1fbe35ac06b2bdf
Stefan Zellmann <info@szellmann.de>
2021-01-02 07:14:28 +0100

    Fix includes

--- src/examples/phantom/main.cpp
+++ src/examples/phantom/main.cpp
@@ -1,6 +1,8 @@
 // This file is distributed under the MIT license.
 // See the LICENSE file for details.
 
+#include <common/config.h>
+
 #include <exception>
 #include <fstream>
 #include <iostream>
