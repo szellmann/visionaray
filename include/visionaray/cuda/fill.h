24a1e705e1ae111306e931a4d178753e3e652dd7
Stefan Zellmann <info@szellmann.de>
2021-01-12 17:47:22 +0100

    Add cuda::fill()

---
+++ include/visionaray/cuda/fill.h
@@ -0,0 +1,14 @@
+// This file is distributed under the MIT license.
+// See the LICENSE file for details.
+
+#pragma once
+
+namespace visionaray
+{
+namespace cuda
+{
+
+void fill(void* ptr, size_t len, void* bytes, unsigned count);
+
+} // cuda
+} // visionaray
---
+++ src/visionaray/cuda/fill.cu
@@ -0,0 +1,52 @@
+// This file is distributed under the MIT license.
+// See the LICENSE file for details.
+
+#include <stdexcept>
+
+#include <visionaray/cuda/fill.h>
+#include <visionaray/math/detail/math.h>
+
+__constant__ char cbytes[1024];
+
+__global__ void kernel(void* ptr, size_t len, unsigned count)
+{
+    size_t i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i >= len / count)
+    {
+        return;
+    }
+
+    for (unsigned c = 0; c < count; ++c)
+    {
+        ((char*)ptr)[i * count + c] = cbytes[c];
+    }
+}
+
+namespace visionaray
+{
+namespace cuda
+{
+
+void fill(void* ptr, size_t len, void* bytes, unsigned count)
+{
+    size_t num_threads = 1024;
+
+    if (count > 1024)
+    {
+        throw std::runtime_error("Fill: max. num bytes exceeded");
+    }
+
+    cudaMemcpyToSymbol(
+            cbytes,
+            bytes,
+            count,
+            0,
+            cudaMemcpyHostToDevice
+            );
+
+    kernel<<<div_up(len / count, num_threads), num_threads>>>(ptr, len, count);
+}
+
+} // cuda
+} // visionaray
