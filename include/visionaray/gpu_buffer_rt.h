bbfb7a2d56c5bbd3261b609427e8587da76354e2
Stefan Zellmann <info@szellmann.de>
2022-02-26 20:14:30 +0100

    Include accum format in ref typ

--- include/visionaray/gpu_buffer_rt.h
+++ include/visionaray/gpu_buffer_rt.h
@@ -29,7 +29,7 @@ public:
     using depth_type    = typename pixel_traits<DepthFormat>::type;
     using accum_type    = typename pixel_traits<AccumFormat>::type;
 
-    using ref_type      = render_target_ref<ColorFormat, DepthFormat>;
+    using ref_type      = render_target_ref<ColorFormat, DepthFormat, AccumFormat>;
 
 public:
 
