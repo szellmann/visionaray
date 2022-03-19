bc68a8689062841eb6134f319b7ea74e2114fef9
Stefan Zellmann <info@szellmann.de>
2022-03-19 19:55:46 +0100

    Don't export image subclasses

--- src/common/pnm_image.h
+++ src/common/pnm_image.h
@@ -8,7 +8,6 @@
 
 #include <string>
 
-#include "export.h"
 #include "image_base.h"
 
 namespace visionaray
@@ -19,15 +18,15 @@ class pnm_image : public image_base
 public:
 
     // Default constructor.
-    VSNRAY_COMMON_EXPORT pnm_image() = default;
+    pnm_image() = default;
 
     // Construct image from width, height, format, and data (data is copied).
-    VSNRAY_COMMON_EXPORT pnm_image(int width, int height, pixel_format format, uint8_t const* data);
+    pnm_image(int width, int height, pixel_format format, uint8_t const* data);
 
-    VSNRAY_COMMON_EXPORT bool load(std::string const& filename);
+    bool load(std::string const& filename);
 
     // Save pnm image. Options: { "binary", <bool> }
-    VSNRAY_COMMON_EXPORT bool save(std::string const& filename, save_options const& options);
+    bool save(std::string const& filename, save_options const& options);
 
 };
 
