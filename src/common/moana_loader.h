814cf79596b8b220cdfb26128331f9805964b739
Stefan Zellmann <info@szellmann.de>
2022-06-18 22:24:15 +0200

    --exports

--- src/common/moana_loader.h
+++ src/common/moana_loader.h
@@ -9,15 +9,13 @@
 #include <string>
 #include <vector>
 
-#include "export.h"
-
 namespace visionaray
 {
 
 class model;
 
-VSNRAY_COMMON_EXPORT void load_moana(std::string const& filename, model& mod);
-VSNRAY_COMMON_EXPORT void load_moana(std::vector<std::string> const& filenames, model& mod);
+void load_moana(std::string const& filename, model& mod);
+void load_moana(std::vector<std::string> const& filenames, model& mod);
 
 } // visionaray
 
