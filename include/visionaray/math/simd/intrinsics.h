bef2dd607d4bd3d080891e936f2d566aa7e51610
Stefan Zellmann <info@szellmann.de>
2024-05-14 20:44:39 +0200

    Add option for user define VSNRAY_NO_SIMD

--- include/visionaray/math/simd/intrinsics.h
+++ include/visionaray/math/simd/intrinsics.h
@@ -51,6 +51,7 @@
 #define VSNRAY_SIMD_ISA_NEON    1010
 #define VSNRAY_SIMD_ISA_NEON_FP 1020
 
+#ifndef VSNRAY_NO_SIMD
 #ifndef VSNRAY_SIMD_ISA_
 #if defined(__AVX512F__)                            && !defined(__CUDACC__) // nvcc does not support AVX intrinsics
 #define VSNRAY_SIMD_ISA_ VSNRAY_SIMD_ISA_AVX512F
@@ -75,6 +76,9 @@
 #else
 #define VSNRAY_SIMD_ISA_ 0
 #endif
+#endif
+#else
+#define VSNRAY_SIMD_ISA_ 0
 #endif
 
 // Undef VSNRAY_SIMD_ISA_ when compiling GPU device code
