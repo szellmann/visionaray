a8d4f6aabfef8e64583c7aafa8eb5da0fa25a975
Stefan Zellmann <info@szellmann.de>
2025-03-04 19:17:46 +0100

    Simplify aligned_allocator

--- include/visionaray/detail/aligned_allocator.h
+++ include/visionaray/detail/aligned_allocator.h
@@ -31,10 +31,7 @@ public:
     typedef T value_type;
     typedef T* pointer;
     typedef const T* const_pointer;
-    typedef T& reference;
-    typedef const T& const_reference;
     typedef size_t size_type;
-    typedef ptrdiff_t difference_type;
 
     aligned_allocator() = default;
 
@@ -53,16 +50,6 @@ public:
         typedef aligned_allocator<U, A> other;
     };
 
-    pointer address(reference r) const
-    {
-        return &r;
-    }
-
-    const_pointer address(const_reference r) const
-    {
-        return &r;
-    }
-
     pointer allocate(size_type n, void* /* hint */ = 0)
     {
         return (pointer)_mm_malloc(n * sizeof(T), A);
@@ -71,21 +58,6 @@ public:
     void deallocate(pointer p, size_type /* n */)
     {
         _mm_free(p);
-    }
-
-    size_t max_size() const
-    {
-        return static_cast<size_t>(-1) / sizeof(T);
-    }
-
-    void construct(pointer p, const_reference val)
-    {
-        new(static_cast<void*>(p)) T(val);
-    }
-
-    void destroy(pointer p)
-    {
-        p->T::~T();
     }
 
     bool operator==(aligned_allocator const& /* rhs */) const
